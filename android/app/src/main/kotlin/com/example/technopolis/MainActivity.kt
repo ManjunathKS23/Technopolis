c380d44fdfd14c07e3f661f66a4e05db33c05508
Dhruthin <dhruthin1234@gmail.com>
2024-07-28 19:41:41 +0530

    Commit

--- android/app/src/main/kotlin/com/example/technopolis/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/technopolis/MainActivity.kt
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 package com.example.technopolis
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+=======
+package com.example.technopolis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
+>>>>>>> origin/main
